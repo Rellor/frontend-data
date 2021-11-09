7b1ea244c90ed9f8830f135a61f3ff5498a34593
Rellor <basderoller@gmail.com>
2021-11-09 10:02:01 +0100

    Circle werkend gekregen in react met d3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import * as d3 from "d3";
+import React, { useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+  useEffect(() => {
+     d3.select(".target").style("stroke-width", 5);
+   }, []);
+   return (
+     <div className="App">
+       <svg>
+         <circle
+           class="target"
+           style={{ fill: "green" }}
+           stroke="black"
+           cx={50}
+           cy={50}
+           r={40}
+         ></circle>
+       </svg>
+     </div>
+   );
+};
 export default App;
